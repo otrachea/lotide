9f15643fb974d918cef775b3a0c8ca28c55e254b
otrachea <francesca.ho459@gmail.com>
2022-03-07 19:23:29 +0000

    Fixed formatting errors

--- head.js
+++ head.js
@@ -1,10 +1,12 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   (actual === expected) ?
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`) :
     console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-const head = (arr) => {return arr[0]};
+const head = (arr) => {
+  return arr[0];
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
