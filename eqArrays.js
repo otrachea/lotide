c4574866f9845d005096c6aef1af90a304d2c0eb
otrachea <francesca.ho459@gmail.com>
2022-03-07 22:58:37 +0000

    Finished eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  (actual === expected) ?
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`) :
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;    
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
