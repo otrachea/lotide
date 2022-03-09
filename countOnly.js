68617bc4674b132052002727b00d8b166b3448ff
otrachea <francesca.ho459@gmail.com>
2022-03-09 05:11:48 +0000

    Finished countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,49 @@
+const assertEqual = function(actual, expected) {
+  (actual === expected) ?
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`) :
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countOnly = (arr, obj) => {
+  
+  let elementCounter = {};
+  let result = {};
+  let keys = Object.keys(obj);
+
+  for (let element of arr) {
+    if (!(element in elementCounter)) {
+      elementCounter[element] = 0;
+    }
+    elementCounter[element]++;
+  }
+
+  for (let element in elementCounter) {
+    if (elementCounter.hasOwnProperty(element)) {
+      if (element in obj && obj[element]) {
+        result[element] = elementCounter[element];
+      }
+    }
+  }
+
+  return result;
+
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
