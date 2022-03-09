1c5eb41abf0cffad2a1c09bf120a3798ed366d54
otrachea <francesca.ho459@gmail.com>
2022-03-09 05:29:19 +0000

    Finished findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  (actual === expected) ?
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`) :
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = (obj, value) => {
+  for (const key in obj) {
+    if (Object.hasOwnProperty.call(obj, key)) {
+      if (obj[key] === value) return key;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sciFi");
+
+const test1 = {
+  a: 1,
+  b: "1"
+}
+
+assertEqual(findKeyByValue(test1, 1), "a");
+assertEqual(findKeyByValue(test1, "1"), "b");
