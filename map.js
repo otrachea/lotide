a0b6f14c33a61845f0fff8b1daae150a637e6a08
otrachea <francesca.ho459@gmail.com>
2022-03-14 18:12:48 +0000

    Refactor map.js

--- map.js
+++ map.js
@@ -1,5 +1,3 @@
-import { assertArraysEqual } from "./assertArraysEqual.js";
-
 const map = (arr, callback) => {
 
   const results = [];
@@ -9,11 +7,4 @@ const map = (arr, callback) => {
   return results;
 };
 
-const words = ["ground", "control", "to", "major", "tom"];
-const results1 = map(words, word => word[0]);
-assertArraysEqual(results1, ['g', 'c', 't', 'm', 't'])
-
-const test1 = [1, 2, 3, 4, 5];
-assertArraysEqual(map(test1, num => num * num), [1, 4, 9, 16, 25]);
-
-assertArraysEqual(map([5, 4, 3, 2, 1], num => num - 5), [0, -1, -2, -3, -4]);
+module.exports = map;
---
+++ test/mapTest.js
@@ -0,0 +1,21 @@
+const assert = require("chai").assert;
+const map = require("../map");
+
+describe("#map", () => {
+  
+  const words = ["ground", "control", "to", "major", "tom"];
+  const results1 = map(words, word => word[0]);
+
+  it("returns ['g', 'c', 't', 'm', 't']", () => {
+    assert.deepEqual(results1, ['g', 'c', 't', 'm', 't']);
+  });
+
+  it("returns [1, 4, 9, 16, 25])", () => {
+    assert.deepEqual(map(test1, num => num * num), [1, 4, 9, 16, 25]);
+  });
+
+  it("returns [0, -1, -2, -3, -4]", () => {
+    assert.deepEqual(map([5, 4, 3, 2, 1], num => num - 5), [0, -1, -2, -3, -4]);
+  })
+
+});
